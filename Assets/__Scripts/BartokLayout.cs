1023858dd5ebbd7b1575a68e458fea948190ff51
Michael Bandola <24bandola@gmail.com>
2017-07-08 17:33:33 -0400

    commit

--- Assets/__Scripts/BartokLayout.cs
+++ Assets/__Scripts/BartokLayout.cs
@@ -3,28 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
-public class SlotDef {
+public class SlotDef{
 	public float x;
 	public float y;
 	public bool faceUp=false;
-	public string layerName= "Default";
-	public int layerID= 0;
+	public string layerName="Default";
+	public int layerID=0;
 	public int id;
-	public List<int> hiddenBy = new List<int>();
-	public string type= "slot";
+	public List<int> hiddenBy=new List<int>();
+	public float rot;
+	public string type="slot";
 	public Vector2 stagger;
+	public int player;
+	public Vector3 pos;
 }
 
-public class Layout : MonoBehaviour {
+public class BartokLayout : MonoBehaviour {
 	public PT_XMLReader xmlr;
 	public PT_XMLHashtable xml;
 	public Vector2 multiplier;
 	public List<SlotDef> slotDefs;
 	public SlotDef drawPile;
 	public SlotDef discardPile;
-	public string[] sortingLayerNames=new string[] { "Row0", "Row1", "Row2", "Row3", "Discard", "Draw"};
+	public SlotDef target;
 
-	public void ReadLayout (string xmlText) {
+	public void ReadLayout(string xmlText){
 		xmlr = new PT_XMLReader ();
 		xmlr.Parse (xmlText);
 		xml = xmlr.xml ["xml"] [0];
@@ -34,40 +37,37 @@ public class Layout : MonoBehaviour {
 		PT_XMLHashList slotsX = xml ["slot"];
 		for (int i = 0; i < slotsX.Count; i++) {
 			tSD = new SlotDef ();
-			if (slotsX [i].HasAtt ("type")) {
+			if (slotsX [1].HasAtt ("type")) {
 				tSD.type = slotsX [i].att ("type");
 			} else {
 				tSD.type = "slot";
 			}
 			tSD.x = float.Parse (slotsX [i].att ("x"));
 			tSD.y = float.Parse (slotsX [i].att ("y"));
+			tSD.pos = new Vector3 (tSD.x * multiplier.x, tSD.y * multiplier.y, 0);
 			tSD.layerID = int.Parse (slotsX [i].att ("layer"));
-			tSD.layerName = sortingLayerNames [tSD.layerID];
+			tSD.layerName = tSD.layerID.ToString ();
 			switch (tSD.type) {
 			case "slot":
-				tSD.faceUp = (slotsX [i].att ("faceup") == "1");
-				tSD.id = int.Parse (slotsX [i].att ("id"));
-				if (slotsX [i].HasAtt ("hiddenby")) {
-					string[] hiding = slotsX [i].att ("hiddenby").Split (',');
-					foreach (string s in hiding) {
-						tSD.hiddenBy.Add (int.Parse (s));
-					}
-				}
-				slotDefs.Add (tSD);
 				break;
-
 			case "drawpile":
 				tSD.stagger.x = float.Parse (slotsX [i].att ("xstagger"));
 				drawPile = tSD;
 				break;
-
 			case "discardpile":
 				discardPile = tSD;
+				break;
+			case "target":
+				target = tSD;
+				break;
+			case "hand":
+				tSD.player = int.Parse (slotsX [i].att ("player"));
+				tSD.rot = float.Parse (slotsX [i].att ("rot"));
+				slotDefs.Add (tSD);
 				break;
 			}
 		}
 	}
-		
 
 	// Use this for initialization
 	void Start () {
